c1e3220fab332ec6d96cce933d6a3c1bda1ddd86
Luong Trung Kien <unholyknightz@gmail.com>
2019-12-10 11:21:53 +0700

    Update backup.sh

--- aerospike/backup.sh
+++ aerospike/backup.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-# Backup single namespace and delete older than 7 days
+# Backup multiple namespace and delete older than 7 days
 # Script by kienlt
+# Required aerospike and aerospike tool installed in this machine
+# namespace folder automatically created
 
 # Init Varriables
 date=$(date -I)
-backup_path=/data1/backup/aerospike/kienlt/
-full_path=$backup_path$date
-namespace=kienlt
+backup_path=/data/backup/
+namespaces=$(aql -o raw -c "show namespaces"  | sort -u | grep -oE "\"(.*)\"" | tr -d "\"")
 
 # Do backup 
-asbackup --namespace $namespace --directory $full_path
+#asbackup --namespace $namespace --directory $full_path
 
 # Remove backup older than 7 days
-find $backup_path* -type d -ctime +7 -exec rm -rf {} \;
+#find $backup_path -type d -ctime +7 -exec rm -rf {} \;
+
+# Do backup 
+for ns in $namespaces
+  do asbackup --namespace $ns --directory ${full_path}${ns}_${date}
+done
